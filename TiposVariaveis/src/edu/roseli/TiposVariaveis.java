1a1775638a23df9217a9db5db77310e089d69e74
roseli-nakaoka <roseli.nakaoka@gmail.com>
2023-09-05 19:07:43 -0300

    Updated TiposVariaveis.java

--- TiposVariaveis/src/edu/roseli/TiposVariaveis.java
+++ TiposVariaveis/src/edu/roseli/TiposVariaveis.java
@@ -3,6 +3,6 @@ package edu.roseli;
 public class TiposVariaveis {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
     }
 }
